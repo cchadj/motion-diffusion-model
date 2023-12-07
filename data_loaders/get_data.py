567bb95c0ef5b6ff80e09962f9b431f2e9eb872a
Chrysostomos <c.chadjiminas@cyens.org.cy>
2023-12-07 11:34:28 +0200

    fix: get_dataset_loader had hml_mode fixed to text_only

--- data_loaders/get_data.py
+++ data_loaders/get_data.py
@@ -41,7 +41,7 @@ def get_dataset(name, num_frames, split='train', hml_mode='train'):
 
 
 def get_dataset_loader(name, batch_size, num_frames, split='train', hml_mode='train'):
-    dataset = get_dataset(name, num_frames, split, hml_mode="text_only")
+    dataset = get_dataset(name, num_frames, split, hml_mode=hml_mode)
     collate = get_collate_fn(name, hml_mode)
     loader = DataLoader(
         dataset, batch_size=batch_size, shuffle=True,

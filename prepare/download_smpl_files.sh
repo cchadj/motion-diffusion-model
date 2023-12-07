e60ab92f6ad32d69c1e2d6ea92db2e0c3f2eca42
Chrysostomos <c.chadjiminas@cyens.org.cy>
2023-12-07 11:32:50 +0200

    fix: add || ext after cd in bash scripts

--- prepare/download_smpl_files.sh
+++ prepare/download_smpl_files.sh
@@ -1,11 +1,11 @@
 mkdir -p body_models
-cd body_models/
+cd body_models/ || exit
 
 echo -e "The smpl files will be stored in the 'body_models/smpl/' folder\n"
 gdown "https://drive.google.com/uc?id=1INYlGA76ak_cKGzvpOV2Pe6RkYTlXTW2"
 rm -rf smpl
 
-unzip smpl.zip
+unzip smpl.zip || exit
 echo -e "Cleaning\n"
 rm smpl.zip
 
